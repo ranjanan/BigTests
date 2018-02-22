17cbdfea4093930bafbeb2e22457741cd40d7467
ranjanan <benditlikeranjan@gmail.com>
2018-02-22 21:03:44 +0530

    Add parallel bench

--- src/script.jl
+++ src/script.jl
@@ -1,7 +1,7 @@
 
-function bench(str, printing = true)
+function bench(str; printing::Bool = true, kind::String = "1-solve")
     !contains(pwd(), str) && cd(joinpath(Pkg.dir(), "BigTests", "data", str))
-    file = "$(str)-1solve.ini"
+    file = "$(str)-$(kind).ini"
     t1 = @elapsed begin
         c = cs.Compute(file, "Screen")
         r1 = c[:compute]()
@@ -11,19 +11,19 @@ function bench(str, printing = true)
     t3 = @elapsed r3 = compute_single(file)
     t4 = @elapsed r4 = compute_cholmod(file)
     printing && println("Python time = $t1")
-    printing && println("Julia serial time = $t2")
+    printing && println("Julia time = $t2")
     printing && println("Julia single precision time = $t3")
     printing && println("Julia cholmod time = $t4")
 end
 
-warmup() = bench("1m", false)
+warmup() = bench("1m", printing = false)
 
-function runall()
+function runall(kind = "1solve")
     info("Warmup computation")
     warmup()
     for str in ["1m", "6m", "12m", "24m", "48m", "96m"]
         info("Benchmarking Problem of Size $str")
-        bench(str)
+        bench(str, kind = kind)
     end
 end
 
